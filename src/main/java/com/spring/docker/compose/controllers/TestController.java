4baacb81036bbcdbba123a0a8a0b4935afe94c71
Mohammad Abdullah Al Mamun <mohammad.mamun@bjitgroup.com>
2022-05-23 17:02:12 +0600

    Third Commit

--- src/main/java/com/spring/docker/compose/controllers/TestController.java
+++ src/main/java/com/spring/docker/compose/controllers/TestController.java
@@ -1,6 +1,10 @@
 package com.spring.docker.compose.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 public class TestController {
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
     public String add(){
         return "Hello World";
     }
